6bbd312ac0ead8b3f31ad86843d97d57a5bcd6d3
Zer02z2 <zongzechan@gmail.com>
2024-10-04 14:34:06 -0400

    Revert "to be discard"
    This reverts commit 818aa96f53d5782c6d7096fe55cbfa12ad611b56.

--- src/fractured-video/sketch.js
+++ src/fractured-video/sketch.js
@@ -1,63 +1,71 @@
-import p5 from "p5"
+var video
 
-let video
-let vScale = 5
-let pixelList = []
+var vScale = 5
 
-const sketch = (p) => {
-  p.setup = () => {
-    p.pixelDensity(1)
-    p.createCanvas(640, 480)
-    video = p.createCapture(p.VIDEO)
-    video.size(p.width / vScale, p.height / vScale)
-    for (var y = 1; y < video.height; y++) {
-      for (var x = 1; x < video.width; x++) {
-        var newPixel = new pixel(p, x, y)
-        pixelList.push(newPixel)
-      }
+var pixelList = []
+
+//var vScale = 5;
+
+function setup() {
+  //frameRate(20);
+  pixelDensity(1)
+  createCanvas(640, 480)
+  video = createCapture(VIDEO)
+  video.size(width / vScale, height / vScale)
+  for (var y = 1; y < video.height; y++) {
+    for (var x = 1; x < video.width; x++) {
+      var newPixel = new pixel(x, y)
+      pixelList.push(newPixel)
     }
-    p.noCursor()
-    video.hide()
   }
+  noCursor()
+  video.hide()
+}
 
-  p.draw = () => {
-    p.background(0)
-    video.loadPixels()
+function draw() {
+  background(0)
+  video.loadPixels()
 
-    for (const i of pixelList) {
-      i.update(p)
-    }
+  for (const i of pixelList) {
+    i.update()
   }
 }
 
-new p5(sketch)
-
 class pixel {
-  constructor(p, ogX, ogY) {
+  constructor(ogX, ogY) {
     this.ogX = ogX
     this.ogY = ogY
     this.index = (video.width - ogX + 1 + ogY * video.width) * 4
     this.speed = 5 / vScale
-    this.xMovement = p.random(0.1, this.speed) * p.random([-1, 1])
+    this.xMovement = random(0.1, this.speed) * random([-1, 1])
     this.yMovement =
-      Math.sqrt(this.speed ** 2 - this.xMovement ** 2) * p.random([-1, 1])
+      Math.sqrt(this.speed ** 2 - this.xMovement ** 2) * random([-1, 1])
     this.x = ogX * vScale
     this.y = ogY * vScale
   }
 
-  update(p) {
+  update() {
     var r = video.pixels[this.index + 0]
     var g = video.pixels[this.index + 1]
     var b = video.pixels[this.index + 2]
 
-    p.noStroke()
-    p.fill(r, g, b)
-    p.rectMode(p.CENTER)
-    p.rect(this.x, this.y, vScale, vScale)
+    //var bright = (r + g + b) / 3;
+    //var w = map(bright, 0, 255, 0, vScale);
+
+    noStroke()
+    fill(r, g, b)
+    rectMode(CENTER)
+    //ellipseMode(CENTER);
+    rect(this.x, this.y, vScale, vScale)
+    //circle(this.x, this.y,10);
 
     if (dist(this.ogX * vScale, this.ogY * vScale, mouseX, mouseY) < 100) {
+      // let [x, y] = [map(this.ogX*vScale - this.x, width, 0, 220, 5),
+      //              map(this.ogY*vScale - this.y, height, 0, 220, 5)]
       this.x = this.ogX * vScale
       this.y = this.ogY * vScale
+      // this.x += x;
+      // this.y += y;
     }
 
     if (this.x < 0 || this.x > width) {
